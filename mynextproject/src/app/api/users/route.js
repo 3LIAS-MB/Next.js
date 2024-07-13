5b0e151093ac8e14241c60c6819a267ff0415756
EliDev <eliasss.mb@gmail.com>
2024-07-13 04:59:06 -0300

    prima

--- mynextproject/src/app/api/users/route.js
+++ mynextproject/src/app/api/users/route.js
@@ -1,4 +1,4 @@
-// Todo esto que hemos escrito tiene el nombre en la documentación de 
+// Todo esto que hemos escrito tiene el nombre en la documentación de
 // Route Handlers. En versiones anteriores a la 13, se llama API Routes
 
 // RUTAS Y CODIGO DE BACKEND, funcionan similar a las de fronted
@@ -8,14 +8,19 @@ export async function GET() {
   // extract params
   // query database
   // communite with other services
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
-  const data = await res.json()
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users?apikey=123${process.env.TOKEN}`);
+  const data = await res.json();
 
   //return new Response("Hello world!")
   return NextResponse.json(data);
 }
 
-export function POST() {
+export async function POST(req) {
+  // request body
+  // const data = await req.json()
+  const { nombre, apellido } = await req.json();
+  console.log(nombre, apellido)
+
   return NextResponse.json({
     message: "Creando datos!",
   });
